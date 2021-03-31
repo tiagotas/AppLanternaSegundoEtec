7ff5c77c831adb9e717853926904e32b8d8a25de
TIAGO SILVA <frost.tiago@gmail.com>
2021-03-31 11:11:33 -0300

    adição de mudança de status da bateria

--- AppLanternaSegundoEtec/AppLanternaSegundoEtec/MainPage.xaml.cs
+++ AppLanternaSegundoEtec/AppLanternaSegundoEtec/MainPage.xaml.cs
@@ -14,13 +14,15 @@ namespace AppLanternaSegundoEtec
             InitializeComponent();
 
             btnOnOff.Source = ImageSource.FromResource("AppLanternaSegundoEtec.Image.botao-desligado.jpg");
+
+            Carrega_Info_Bateria();
         }
 
         private void btnOnOff_Clicked(object sender, EventArgs e)
         {
             try
             {
-                if(lanterna_ligada)
+                if (lanterna_ligada)
                 {
                     // desligar
                     btnOnOff.Source = ImageSource.FromResource("AppLanternaSegundoEtec.Image.botao-desligado.jpg");
@@ -29,7 +31,8 @@ namespace AppLanternaSegundoEtec
                     Flashlight.TurnOffAsync();
                     Vibration.Vibrate(TimeSpan.FromMilliseconds(300));
 
-                } else
+                }
+                else
                 {
                     // ligar
                     btnOnOff.Source = ImageSource.FromResource("AppLanternaSegundoEtec.Image.botao-ligado.jpg");
@@ -37,11 +40,106 @@ namespace AppLanternaSegundoEtec
 
                     Flashlight.TurnOnAsync();
                     Vibration.Vibrate(TimeSpan.FromMilliseconds(300));
-                }             
-            } catch(Exception ex)
+                }
+            }
+            catch (Exception ex)
             {
-                DisplayAlert("Ops", ex.Message, "OK :(" );
+                DisplayAlert("Ops", ex.Message, "OK :(");
             }
         }
+
+        private void Carrega_Info_Bateria()
+        {
+            try
+            {
+                if (CrossBattery.IsSupported)
+                {
+                    CrossBattery.Current.BatteryChanged -= Mudanca_Status_Bateria;
+                    CrossBattery.Current.BatteryChanged += Mudanca_Status_Bateria;
+
+                }
+                else
+                {
+                    throw new Exception("Sem permissão para dados da bateria.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Ok");
+            }
+        }
+
+
+        private void Mudanca_Status_Bateria(object sender, Plugin.Battery.Abstractions.BatteryChangedEventArgs e)
+        {
+            // O que mudou?
+            // Carga Remancente d Bateria
+            // Status da Bateria
+            // Fonte de Energia
+            try
+            {
+                lbl_carga.Text = e.RemainingChargePercent.ToString() + "%";
+
+                // Escolha - Caso
+
+                // Qual é o status da bateria? Carregando? Descarregando?
+                switch (e.Status)
+                {
+                    case Plugin.Battery.Abstractions.BatteryStatus.Charging:
+                        lbl_status.Text = "Carregando";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Discharging:
+                        lbl_status.Text = "Descarregando";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Full:
+                        lbl_status.Text = "Carregada";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.NotCharging:
+                        lbl_status.Text = "Sem Carregar";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Unknown:
+                        lbl_status.Text = "Desconhecido";
+                        break;
+                }
+
+                // Qual é a fonte de energia da bateria?
+                switch(e.PowerSource)
+                {
+                    case Plugin.Battery.Abstractions.PowerSource.Ac:
+                        lbl_fonte.Text = "Carregador";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Battery:
+                        lbl_fonte.Text = "Bateria";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Usb:
+                        lbl_fonte.Text = "USB";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Wireless:
+                        lbl_fonte.Text = "Sem fio";
+                        break;
+
+                    case Plugin.Battery.Abstractions.PowerSource.Other:
+                        lbl_fonte.Text = "Outro";
+                        break;
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Ok");
+            }
+        }
+
+
+
     }
 }
